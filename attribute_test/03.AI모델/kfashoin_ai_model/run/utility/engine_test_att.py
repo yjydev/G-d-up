38a6a10d987d7f264c4b0dc8cbacd60c5fd17473
김유진 <determination9212@gmail.com>
2022-01-31 16:13:11 +0900

    fix : Update argument
    'cuda()' no longer has an argument async.
    It is replaced by non_blocking.
    https://stackoverflow.com/questions/53201534/calling-cuda-with-async-results-in-syntaxerror
    
    Former-commit-id: 1744ded2b59442b5ce3cbb09a5d7d19a3eb5e57d

--- attribute_test/03.AI모델/kfashoin_ai_model/run/utility/engine_test_att.py
+++ attribute_test/03.AI모델/kfashoin_ai_model/run/utility/engine_test_att.py
@@ -179,7 +179,7 @@ class Engine(object):
             self.on_start_batch(False, model, criterion, data_loader)
 
             if self.state['use_gpu']:
-                self.state['target'] = self.state['target'].cuda(async=True)
+                self.state['target'] = self.state['target'].cuda(non_blocking=True)
 
             self.on_forward(False, model, criterion, data_loader)
 
