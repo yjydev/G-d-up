8b5fe68ff8fbcb81e858141d8bf5eeb704dd2f92
woori9 <wrh95222@gmail.com>
2022-02-08 00:06:37 +0900

    test : category 컴포넌트 테스트 추가
    Former-commit-id: 278ff1febe5c14b79112d4d48bd4ec43984c178f

--- frontend/src/components/categories/Categories.test.jsx
+++ frontend/src/components/categories/Categories.test.jsx
@@ -6,12 +6,17 @@ import React from 'react';
 import { render } from "@testing-library/react";
 import Categories from './Categories.jsx';
 
+const handleClick = jest.fn();
+
 describe('Categories', () => {
   it('renders Categories', () => {
-
     const { getByText } = render((
-      <Categories />
-    ))
+      <Categories
+        categories={[]}
+        selectedCategory={'전체'}
+        handleClick={handleClick}
+      />
+    ));
 
     expect(getByText(/Nav/)).not.toBeNull();
   });
